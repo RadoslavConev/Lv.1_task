d94ba19366ef8cf645237d2145f3d6cae85a0509
RadoslavConev <rts.conev@gmail.com>
2021-01-29 12:30:56 +0200

    Update final_task.php

--- final_task.php
+++ final_task.php
@@ -1,49 +1,29 @@
 <?php
 
-$arr = ["abc", 
- "a", 
- "b", 
- "c", 
- "def", 
- "g", 
- "ghi"
-];
+$arr = [ ];
 $arr_count = count($arr) - 1;
-$raw_result = 0;
-
-for ($k=0; $k < $arr_count; $k++) { 
-	$front = substr($arr[$k+1], 0, 1);
-	$rear = substr($arr[$k], -1);
-		if ($front != $rear) {
-			$raw_result++;
-		}
-}
-
-$replaced_result = 0;
+$result = 0;
 
 for ($i=0; $i < $arr_count; $i++) { 
 	$front = substr($arr[$i+1], 0, 1);
 	$rear = substr($arr[$i], -1);
-		if ($front != $rear) {
+		if ( ($i + 2) <= $arr_count ) {
+			$next_front = substr($arr[$i + 2], 0, 1);
+			$next_rear = substr($arr[$i + 1], -1);
+			}
+	if ($front != $rear && $next_rear != $next_front) {
 			$arr[$i + 1] = substr_replace($arr[$i + 1], $rear, 0, 1);
-			$replaced_result++;
+			$result++;
+		}elseif ($front != $rear && $next_rear == $next_front) {
+			if ( strlen($arr[$i + 1]) == 1 ){
+				$arr[$i] = substr_replace($arr[$i], $next_rear, -1);
+			}elseif (strlen($arr[$i + 1]) >= 2 ) {
+				$arr[$i + 1] = substr_replace($arr[$i + 1], $rear, 0, 1);
+			}
+			$result++;
 		}
-	echo "<pre>";
-	var_dump($front);
-	var_dump($rear);
-	var_dump($arr);
-	echo "</pre>";
+	$next_front = "1";
+	$next_rear = "2";
 }
-//var_dump($front);
-//var_dump($rear);
-//echo $result;
 
-if ($raw_result == $replaced_result) {
-	echo 'raw result (1) ' . $replaced_result;
-}elseif ($replaced_result > $raw_result) {
-	$result = $replaced_result - $raw_result;
-	echo "(2) " . $result;
-}elseif ($raw_result > $replaced_result) {
-	$result = $raw_result - $replaced_result;
-	echo "(3) " . $result;
-}
+echo $result;
